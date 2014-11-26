84ef9a5e915c7f1ced51c21f97b399343ec14fd4
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-11-26 16:01:54 +0800

    Add demo buttons.

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,9 @@
 function onDeviceReady() {
-    $("#app-status-ul").prepend('<li>deviceready event received</li>');
-
-    document.addEventListener("backbutton", function(e) {
-        $("#app-status-ul").prepend('<li>backbutton event received</li>');
-
-        if ($("#home").length > 0) {
-            // call this to get a new token each time. don't call it to reuse existing token.
-            //pushNotification.unregister(successHandler, errorHandler);
-            e.preventDefault();
-            navigator.app.exitApp();
-        } else {
-            navigator.app.backHistory();
-        }
-    }, false);
+    $("#debug").prepend('<li>deviceready event received</li>');
 
     try {
         pushNotification = window.plugins.pushNotification;
-        $("#app-status-ul").prepend('<li>registering ' + device.platform + '</li>');
+        $("#debug").prepend('<li>registering ' + device.platform + '</li>');
         if (device.platform == 'android' || device.platform == 'Android' ||
             device.platform == 'amazon-fireos') {
             pushNotification.register(successHandler, errorHandler, {
@@ -41,7 +28,7 @@ function onDeviceReady() {
 // handle APNS notifications for iOS
 function onNotificationAPN(e) {
     if (e.alert) {
-        $("#app-status-ul").prepend('<li>MESSAGE -> MSG: ' + e.alert + '</li>');
+        $("#debug").prepend('<li>MESSAGE -> MSG: ' + e.alert + '</li>');
         // showing an alert also requires the org.apache.cordova.dialogs plugin
         navigator.notification.alert(e.alert);
     }
@@ -59,12 +46,12 @@ function onNotificationAPN(e) {
 
 // handle GCM notifications for Android
 function onNotification(e) {
-    $("#app-status-ul").prepend('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+    $("#debug").prepend('<li>EVENT -> RECEIVED:' + e.event + '</li>');
 
     switch (e.event) {
         case 'registered':
             if (e.regid.length > 0) {
-                $("#app-status-ul").prepend('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+                $("#debug").prepend('<li>REGISTERED -> REGID:' + e.regid + "</li>");
                 // Your GCM push server needs to know the regID before it can push to this device
                 // here is where you might want to send it the regID for later use.
                 console.log("regID = " + e.regid);
@@ -87,7 +74,7 @@ function onNotification(e) {
             // if this flag is set, this notification happened while we were in the foreground.
             // you might want to play a sound to get the user's attention, throw up a dialog, etc.
             if (e.foreground) {
-                $("#app-status-ul").prepend('<li>--INLINE NOTIFICATION--' + '</li>');
+                $("#debug").prepend('<li>--INLINE NOTIFICATION--' + '</li>');
 
                 // on Android soundname is outside the payload. 
                 // On Amazon FireOS all custom attributes are contained within payload
@@ -99,26 +86,26 @@ function onNotification(e) {
                 my_media.play();
             } else { // otherwise we were launched because the user touched a notification in the notification tray.
                 if (e.coldstart)
-                    $("#app-status-ul").prepend('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                    $("#debug").prepend('<li>--COLDSTART NOTIFICATION--' + '</li>');
                 else
-                    $("#app-status-ul").prepend('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+                    $("#debug").prepend('<li>--BACKGROUND NOTIFICATION--' + '</li>');
             }
 
-            $("#app-status-ul").prepend('<li>MESSAGE -> MSG: ' + e.payload.alert + '</li>');
+            $("#debug").prepend('<li>MESSAGE -> MSG: ' + e.payload.alert + '</li>');
             break;
 
         case 'error':
-            $("#app-status-ul").prepend('<li>ERROR -> MSG:' + e.msg + '</li>');
+            $("#debug").prepend('<li>ERROR -> MSG:' + e.msg + '</li>');
             break;
 
         default:
-            $("#app-status-ul").prepend('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+            $("#debug").prepend('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
             break;
     }
 }
 
 function tokenHandler(result) {
-    $("#app-status-ul").prepend('<li>REGISTERED -> REGID:' + result + "</li>");
+    $("#debug").prepend('<li>REGISTERED -> REGID:' + result + "</li>");
     // Your iOS push server needs to know the token before it can push to this device
     // here is where you might want to send it the token for later use.
     console.log("regID = " + result);
@@ -137,11 +124,22 @@ function tokenHandler(result) {
 }
 
 function successHandler(result) {
-    $("#app-status-ul").prepend('<li>success:' + result + '</li>');
+    $("#debug").prepend('<li>success:' + result + '</li>');
 }
 
 function errorHandler(error) {
-    $("#app-status-ul").prepend('<li>error:' + error + '</li>');
+    $("#debug").prepend('<li>error:' + error + '</li>');
 }
 
 document.addEventListener('deviceready', onDeviceReady, true);
+
+// Handle scope button debug.
+['demoscope1', 'demoscope2', 'demoscope3', 'demoscope4'].forEach(function(element, index, array) {
+    $(document).on('click', '#' + element, function() {
+        if ($('#' + element).hasClass('active')) {
+            $("#debug").prepend('<li>' + element + ' active</li>');
+        } else {
+            $("#debug").prepend('<li>' + element + ' deactive</li>');
+        }
+    });
+});
