ceb98fbcceefaf11d985ef82d3fbba12d998a132
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-12-21 03:54:51 +0800

    Fix oauth2 authorize path.

--- src/js/authbucket.js
+++ src/js/authbucket.js
@@ -8,7 +8,7 @@
             base: 'http://oauth2-symfony-bundle.authbucket.com/api/oauth2/',
             oauth: {
                 version: 2,
-                auth: 'http://oauth2-symfony-bundle.authbucket.com/api/oauth2/authorize',
+                auth: 'http://oauth2-symfony-bundle.authbucket.com/demo/authorize',
                 grant: 'http://oauth2-symfony-bundle.authbucket.com/api/oauth2/token',
             },
             refresh: true,
